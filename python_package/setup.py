0325e6649b0fb01a36f3dd774bb95b7910958093
BOUHAFSADMI <sadmibouhafs@gmail.com>
2020-06-13 21:32:59 +0100

    feat: Create a python package
    - Create General and Gaussian classes
    
    - Setup of __init__.py & setup.py

---
+++ python_package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name="distributions",
+      version=1.0,
+      description="Probability distribution",
+      packages=["distributions"],
+      zip_safe=False)
